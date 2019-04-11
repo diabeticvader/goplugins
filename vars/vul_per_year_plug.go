4afe0dfd07f466776017e779b22338c7e9cdfc0e
dv <diabeticvader@protonmail.com>
2019-04-11 16:55:13 -0400

    more changes 4/11/2019

--- vars/vul_per_year_plug.go
+++ vars/vul_per_year_plug.go
@@ -30,21 +30,13 @@ func GenerateReport() (string, error) {
             `
 	a := 0
 	b := 0
+	// Call get open and close vulnerabilities
 
 	s := fmt.Sprintf("<tr><td>%d</td><td>%d</td></tr>", a, b)
 	return fmt.Sprintf(html, s), nil
 }
 
-// GetVuln gets the open and closed vulnerabilities
-func GetVuln() {
-
-}
-
 // Count counts the number of vulnerabilities per Year
 func Count() {
-
-}
-
-func main() {
 
 }

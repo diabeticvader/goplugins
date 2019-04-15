c5a7d8379fca9909a3fc287be154ee476e9eb94b
dv <diabeticvader@protonmail.com>
2019-04-15 13:02:09 -0400

    adding the final plugin && .so ext is required when uploading to server

--- vars/vul_per_year_plug.go
+++ vars/vul_per_year_plug.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	//"time"
+
+	"github.com/cbelk/vars/pkg/varsapi"
 	//"github.com/wcharczuk/go-chart"
-	//"github.com/cbelk/vars/pkg/varsapi"
 )
 
 // Name the report
@@ -18,8 +18,8 @@ func GenerateReport() (string, error) {
             <table class="table table-striped table-dark table-bordered table-hover" id="num-table">
                 <thead>
                     <tr>
-                        <th scope="col" class="col-3" onclick="sortTable('num-table', 0)">2018</th>
-                        <th scope="col" class="col-3" onclick="sortTable('num-table', 1)">2019</th>
+                        <th scope="col" class="col-3" onclick="sortTable('num-table', 0)">Year</th>
+                        <th scope="col" class="col-3" onclick="sortTable('num-table', 1)">Count</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -28,15 +28,26 @@ func GenerateReport() (string, error) {
             </table>
             </div>
             `
-	a := 0
-	b := 0
-	// Call get open and close vulnerabilities
+	// Get all vulnerabilities
+	vulns, err := varsapi.GetVulnerabilities()
+	if !varsapi.IsNilErr(err) {
+		return "", err
+	}
 
-	s := fmt.Sprintf("<tr><td>%d</td><td>%d</td></tr>", a, b)
+	count := make(map[int]int)
+
+	for _, v := range vulns {
+		year, _, _ := v.Dates.Initiated.Date()
+		_, ok := count[year]
+		if ok {
+			count[year]++
+		} else {
+			count[year] = 1
+		}
+	}
+	s := ""
+	for y, c := range count {
+		s += fmt.Sprintf("<tr><td>%d</td><td>%d</td></tr>", y, c)
+	}
 	return fmt.Sprintf(html, s), nil
-}
-
-// Count counts the number of vulnerabilities per Year
-func Count() {
-
 }
